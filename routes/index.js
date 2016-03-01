520f172df9ab9e608622ac773b414eedd28f25cf
Shweta <Shweta@lawn-128-61-55-112.lawn.gatech.edu>
2016-03-01 13:05:10 -0500

    post get request test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//// this is the post request for the caster
+app.post('/api/cast', function(req, res) {
+
+  //res.render('index', { title: 'Express' });
+    console.log("printing the json for caster");
+    res.send("Got a POST REQUEST");
+    console.log(res);
+
+});
+
+
+// this is the get request for the asker to get the feed
+router.get('/api/ask', function(req, res) {
+
+    //res.render('index', { title: 'Express' });
+    console.log("printing the json for asker");
+    console.log(req);
+    console.log(res);
+
+});
+
 module.exports = router;
