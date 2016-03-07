0b8ae58a3a2873f0f671d27fa86a46a2bc238163
Nikhil Bhanu <nikhil.bhanu28@gmail.com>
2016-03-06 23:48:40 -0500

    folder structure for templates, js and css. removed django and node

--- Meteor/todos/server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-	service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-	service: 'facebook',
-	appId: '250066481994039',
-	secret: '919c98c7ac140d0e39633e79704130c1'
-});
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+	service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+	service: 'facebook',
+	appId: '250066481994039',
+	secret: '919c98c7ac140d0e39633e79704130c1'
+});
