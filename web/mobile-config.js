d56882565d8b0613429400b49225295d3d3d4706
Shweta <Shweta@lawn-128-61-59-38.lawn.gatech.edu>
2016-04-03 18:54:14 -0400

    added ios splases

--- web/mobile-config.js
+++ web/mobile-config.js
@@ -29,13 +29,13 @@ App.icons({
 
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/splash-320x480.png',
+  'iphone': 'resources/our_splash/splash-320x480.png',
   'iphone_2x': 'resources/splash/splash-320x480@2x.png',
   'iphone5': 'resources/splash/splash-320x568@2x.png',
-  'ipad_portrait': 'resources/splash/splash-768x1024.png',
-  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-  'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+  'ipad_portrait': 'resources/our_splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'resources/our_splash/splash-768x1024@2x.png',
+  'ipad_landscape': 'resources/our_splash/splash-1024x768.png',
+  'ipad_landscape_2x': 'resources/our_splash/splash-1024x768@2x.png',
 
   // Android
   // 'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
